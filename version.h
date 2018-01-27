5f91cab603ea7af59cd67f311c0322974dd2c0a4
Kravchenko Andrey <epozine@inbox.ru>
2018-01-27 13:47:26 +0300

    Add script and version file

---
+++ version.h
@@ -0,0 +1,32 @@
+/*
+ * Класс, описывающий простую дробь
+*/
+class simple_fraction
+{
+public:
+    simple_fraction(int numerator, int denominator)
+    {
+        if (denominator == 0) // Ошибка деления на ноль
+            throw std::runtime_error("zero division error");
+        this->numerator = numerator;
+        this->denominator = denominator;
+    }
+#define version_build 19
+    // Определение основных математических операций для простой дроби
+    double operator+ (int val) { return number() + val; } // Сложение
+    double operator- (int val) { return number() - val; } // Вычитание
+    double operator* (int val) { return number() * val; } // Умножение
+    double operator/ (int val) // Деление
+    {
+        if (val == 0) {
+            throw std::runtime_error("zero division error");
+        }
+        return number() / val;
+    }
+
+    // Получение значения дроби в виде обычного double-числа
+    double number() { return numerator / (double) denominator; }
+private:
+    int numerator; // Числитель
+    int denominator; // Знаменатель
+};  
