26d55015550830317c2be6c6eecca6f8618a07d8
Your New Name <Your new email>
2018-01-31 02:25:17 -0800

    1

--- version.h
+++ version.h
@@ -13,7 +13,7 @@ public:
         this->denominator = denominator;
     }
 
-#define version_build 2.3.30.212
+#define version_build 2.3.30.21233
     // Определение основных математических операций для простой дроби
     double operator+ (int val) { return number() + val; } // Сложение
     double operator- (int val) { return number() - val; } // Вычитание
